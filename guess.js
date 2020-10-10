92aa26ab5f5b423e08d5b4de2ec55052ace57a40
jaromeaviles <jaromeaviles@gmail.com>
2020-10-10 13:19:57 +0800

    Update: Refactor Script, Added Too High and Too Low

--- guess.js
+++ guess.js
@@ -1,44 +1,29 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 let input = document.querySelector("input");
 let enterButton = document.querySelector("#enter");
 let resetButton = document.querySelector("#reset");
-let displayMessage = document.getElementById("displayMessage");
+let displayMessage = document.querySelector("p");
 
-let answer = randomNumbers();
+let answer = Math.floor(Math.random() * 100);
 
 enterButton.addEventListener("click", function () {
 
-if (Number(input.value) === answer) {
-	displayMessage.textContent = "You've Guessed It!";
-	displayMessage.classList.add("message");
+if (input.value === "") {
+	displayMessage.textContent = "Field is empty :(";
+} else if (Number(input.value) < answer) {
+	displayMessage.textContent = "It's too low!";
+} else if (Number(input.value) > answer) {
+	displayMessage.textContent = "It's too high!";
+} else {
+	displayMessage.textContent = "You've Guessed It! and you picked number is " + answer + "!";
 	enterButton.disabled = true;
 	input.classList.add("disabled");
-
-} else if (input.value === "") {
-
-	displayMessage.textContent = "Field is empty :(";
-
-} else {
-
-	displayMessage.textContent = "Error! Try Again!";
-	displayMessage.classList.add("message");
 }
 
 });
 
 resetButton.addEventListener("click", function () {
 	input.value = "";
-	displayMessage.textContent = "";
-	displayMessage.classList.remove("message");
+	displayMessage.textContent = "Pick a number from 1-100";
 	enterButton.disabled = false;
-	answer = randomNumbers();
+	answer = Math.floor(Math.random() * 100);
 });
-
-function randomNumbers () {
-	for (let i = 1; i < numbers.length; i++) {
-		let random = Math.floor(Math.random() * numbers.length + 1);
-		return random;
-	}
-}
-
